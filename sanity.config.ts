e2e48a14227488c6dd1ac213edea51822ae27ca3
Khalil-NOUI <NOUI.KHALIL.PR@GMAIL.COM>
2023-12-08 17:34:35 +0100

    Initial commit

---
+++ sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'TempFolio',
+
+  projectId: 'h0qt4w6i',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
