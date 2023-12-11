80cbeb5ebfce03f80a7de1541740f30a3d727cb0
Khalil-NOUI <NOUI.KHALIL.PR@GMAIL.COM>
2023-12-11 10:43:19 +0100

    enhance style

--- app/(site)/ui/Navbar.tsx
+++ app/(site)/ui/Navbar.tsx
@@ -4,17 +4,19 @@ import Link from "next/link";
 import NavLinks from '../lib/navLinks';
 
 export default function Navbar() {
+  const kn = "< KN />"
   return (
     <header className="py-5 md:px-16 px-6 z-30 md:mb-28 mb-20">
       <div className="max-w-6xl mx-auto flex items-center justify-between">
          <Link href="/"
-         className="text-lg font-extrabold ">
-          <Image 
+         className="text-xl font-extrabold ">
+          {/* <Image 
           src="/logo.png"
-          width={50}
-          height={50}
+          width={40}
+          height={40}
           className='hidden md:block'
-          alt='Logo'/>
+          alt='Logo'/> */}
+          {kn}
         </Link>
         <nav>
          <NavLinks />
